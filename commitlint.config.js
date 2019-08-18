f09886532b2b6cfd784b6d53255970577ab1a24c
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2019-08-18 16:00:20 -0400

    chore: Update CD (#14)
    * chore: update rules for commitlint & release-it
    
    * chore: make naming consistent for releases

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,17 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "type-enum": [2, "always", [
+            'feat',
+            'chore',
+            'fix',
+            'docs',
+            'style',
+            'refactor',
+            'test',
+            'revert',
+            'release'
+        ]
+        ]
+    }
+};
