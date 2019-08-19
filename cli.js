a2c7ea697d76b8fe5577a1f6422e3ba430cc7526
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2019-08-18 22:20:02 -0400

    refactor: clarify cli param

--- cli.js
+++ cli.js
@@ -5,8 +5,8 @@ const package = require('./package.json');
 
 program.version(package.version);
 program
-    .option('-u, --url <url>', 'starting valid url for auto-lighthouse')
-    .option('-o, --open <open>', 'boolean for auto opening reports')
+    .option('-u, --url <url>', 'starting valid url for auto-lighthouse', 'https://blank.org')
+    .option('-e, --express <open>', 'flag for auto opening reports in local express server')
     .option('-p, --port <port>', 'port for local express server', 9000);
 
 program.parse(process.argv);
