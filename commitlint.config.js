b3323fa34cbb5849197f1c45be077b64b1db3d0e
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2019-09-10 22:39:24 -0400

    refactor: allow build commit, remove chore

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,8 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         "type-enum": [2, "always", [
+            'build',
             'feat',
-            'chore',
             'fix',
             'docs',
             'style',
