c3a6f6017caa7131bdbce7f980504e72462e5b38
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2020-05-29 20:31:46 -0400

    feat: show help if cli is called with no options

--- cli.js
+++ cli.js
@@ -6,16 +6,18 @@ const package = require('./package.json');
 const program = new commander.Command();
 program.version(package.version);
 program
-    .option('-u, --url <urls>', 'starting valid url(s) for auto-lighthouse',
-        [
-            'https://tgiles.github.io'
-        ])
+    .option('-u, --url <urls>', 'starting valid url(s) for auto-lighthouse')
     .option('--format <mode>', 'output format of Lighthouse reports. Allowed values: html, json, csv.', 'html')
     .option('-e, --express <open>', 'flag for auto opening reports in local express server. Allowed values: true, false.')
     .option('-p, --port <port>', 'port for local express server', 9000);
 
 program.parse(process.argv);
-lighthouseRunner.main(program);
+if (program.rawArgs.length === 2) {
+    program.help();
+}
+else {
+    lighthouseRunner.main(program);
+}
 
 module.exports = {
     program
