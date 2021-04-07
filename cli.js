4e42b0fde18f1b3f5cd8e308b4276f453f840b47
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2021-04-07 19:24:26 -0400

    fix: fix emulation flags due to Lighthouse v7 update (#233)
    * fix: add missing device option in CLI
    
    * fix: fix emulation flags due to Lighthouse v7

--- cli.js
+++ cli.js
@@ -7,6 +7,7 @@ const program = new commander.Command();
 program.version(package.version);
 program
     .option('-u, --url <urls...>', 'starting valid url(s) for auto-lighthouse')
+    .option('-d, --device <device>', 'Used if only desktop or only mobile reports are needed. Allow values: mobile, desktop')
     .option('--format <mode>', 'output format of Lighthouse reports. Allowed values: html, json, csv.', 'html')
     .option('-e, --express <open>', 'flag for auto opening reports in local express server. Allowed values: true, false.')
     .option('-t, --threads <threads>', 'Number of threads used. Defaults to all threads.')
